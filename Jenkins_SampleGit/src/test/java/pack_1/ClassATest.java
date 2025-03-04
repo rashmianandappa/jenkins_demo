2c3727725e8325a52fc46d54bb35e645009dcc86
rashmianandappa <rashmi.ba512@gmail.com>
2025-03-04 11:10:36 +0530

    First Commit

---
+++ Jenkins_SampleGit/src/test/java/pack_1/ClassATest.java
@@ -0,0 +1,11 @@
+package pack_1;
+
+import org.testng.annotations.Test;
+
+public class ClassATest {
+	@Test
+	public void sampletest() {
+		System.out.println("hiiiiii");
+	}
+
+}
---
+++ Jenkins_SampleGit/src/test/java/pack_1/ClassBTest.java
@@ -0,0 +1,11 @@
+package pack_1;
+
+import org.testng.annotations.Test;
+
+public class ClassBTest {
+	@Test
+	public void sampletest() {
+		System.out.println("Bye");
+	}
+
+}
